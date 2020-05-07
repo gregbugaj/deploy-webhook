b8eeac13b4ee88fb8fa2026b8249af42d0bb8369
Greg <greg@odsy.io>
2020-05-07 08:44:40 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo 'Stopping existing service'
+sudo systemctl stop deploy-webhook.service
+
+echo "Building"
+
+go build
+sudo cp deploy-webhook /usr/local/bin
+rm ./deploy-webhook
+
+echo "Setup directory '/etc/deploy-webhook/'"
+sudo mkdir -p /etc/deploy-webhook
+sudo chown app-svc:app-svc /etc/deploy-webhook
+cp ./deploy-ansible.sh /etc/deploy-webhook/deploy.sh
+
+
+#sudo chown app-svc:app-svc /etc/deploy-webhook
+
+echo 'Installing service : deploy-webhook.service'
+sudo cp deploy-webhook.service /etc/systemd/system/deploy-webhook.service
+
+echo '------ Service commands --------'
+echo 'Service start : sudo systemctl start deploy-webhook.service'
+echo 'Service status: sudo systemctl status deploy-webhook.service'
+
+echo 'Starting services'
+sudo systemctl start deploy-webhook.service
+sudo systemctl status deploy-webhook.service
+
